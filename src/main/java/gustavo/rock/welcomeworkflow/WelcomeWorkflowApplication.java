f9198ab71f5b995bea4de495ce49877356dcf00c
gustavtjac <gustavtorup@gmail.com>
2025-02-26 08:55:10 +0100

    inmit commit

---
+++ src/main/java/gustavo/rock/welcomeworkflow/WelcomeWorkflowApplication.java
@@ -0,0 +1,13 @@
+package gustavo.rock.welcomeworkflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WelcomeWorkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gustavo/rock/welcomeworkflow/WelcomeWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package gustavo.rock.welcomeworkflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
